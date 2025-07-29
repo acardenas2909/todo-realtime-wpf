6acce8fe3c38ffa2b079a8f4f23d6397543e7b00
acardenasdv <acardenas@datavoice.com.mx>
2025-07-28 18:53:09 -0600

    Add project files.

---
+++ WpfTodoApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfTodoApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
